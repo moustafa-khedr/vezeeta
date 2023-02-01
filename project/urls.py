1290ebe3327f774d62c0df636b29a1f3624d61c8
Moustafa_Khedr <mustfakhedr1998@gmail.com>
2023-02-02 00:27:38 +0200

    after add base.html and do extend,include

--- project/urls.py
+++ project/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import include, path
+
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls', namespace = 'accounts')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
